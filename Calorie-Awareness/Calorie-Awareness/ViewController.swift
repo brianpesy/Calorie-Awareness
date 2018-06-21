ecc2a483bd6e0ffe1b02d6ae5e6a7b7a71f53154
Brian Sy <brianpesy@gmail.com>
2018-06-21 16:30:44 +0800

    Initial project

---
+++ Calorie-Awareness/Calorie-Awareness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calorie-Awareness
+//
+//  Created by Brian Sy on 21/06/2018.
+//  Copyright © 2018 BrianSy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Calorie-Awareness/Calorie-AwarenessTests/Calorie_AwarenessTests.swift
@@ -0,0 +1,36 @@
+//
+//  Calorie_AwarenessTests.swift
+//  Calorie-AwarenessTests
+//
+//  Created by Brian Sy on 21/06/2018.
+//  Copyright © 2018 BrianSy. All rights reserved.
+//
+
+import XCTest
+@testable import Calorie_Awareness
+
+class Calorie_AwarenessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
